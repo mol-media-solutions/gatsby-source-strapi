8c3c3eea87b6965e64ec71df242a2a86b9c490e2
Yannick Mol <yannick@molmediasolutions.nl>
2020-10-07 18:00:19 +0200

    Revert "add eslint"
    This reverts commit c681bcbc7a2c4353015dc957eb8540ba4484ad33.

--- src/normalize.js
+++ src/normalize.js
@@ -1,12 +1,24 @@
 import { has } from 'lodash/fp'
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
-const extractFields = async (apiURL, store, cache, createNode, touchNode, auth, item) => {
+const extractFields = async (
+  apiURL,
+  store,
+  cache,
+  createNode,
+  touchNode,
+  auth,
+  item
+) => {
   for (const key of Object.keys(item)) {
     const field = item[key]
     if (Array.isArray(field)) {
       // add recursion to fetch nested strapi references
-      await Promise.all(field.map(async f => extractFields(apiURL, store, cache, createNode, touchNode, auth, f)))
+      await Promise.all(
+        field.map(async f =>
+          extractFields(apiURL, store, cache, createNode, touchNode, auth, f)
+        )
+      )
     } else {
       // image fields have a mime property among other
       // maybe should find a better test
@@ -27,7 +39,9 @@ const extractFields = async (apiURL, store, cache, createNode, touchNode, auth, 
         if (!fileNodeID) {
           try {
             // full media url
-            const source_url = `${field.url.startsWith('http') ? '' : apiURL}${field.url}`
+            const source_url = `${field.url.startsWith('http') ? '' : apiURL}${
+              field.url
+            }`
             const fileNode = await createRemoteFileNode({
               url: source_url,
               store,
@@ -60,12 +74,28 @@ const extractFields = async (apiURL, store, cache, createNode, touchNode, auth, 
 }
 
 // Downloads media from image type fields
-exports.downloadMediaFiles = async ({ entities, apiURL, store, cache, createNode, touchNode, jwtToken: auth }) =>
+exports.downloadMediaFiles = async ({
+  entities,
+  apiURL,
+  store,
+  cache,
+  createNode,
+  touchNode,
+  jwtToken: auth,
+}) =>
   Promise.all(
     entities.map(async entity => {
       for (let item of entity) {
         // loop item over fields
-        await extractFields(apiURL, store, cache, createNode, touchNode, auth, item)
+        await extractFields(
+          apiURL,
+          store,
+          cache,
+          createNode,
+          touchNode,
+          auth,
+          item
+        )
       }
       return entity
     })
